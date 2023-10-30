e7794002322e5d12e694b2ed882fcb2fab107a6a
Christopher Forti <christopher.forti.79@gmail.com>
2023-10-30 18:54:44 -0400

    Initial commit

--- nano.sh
+++ nano.sh
@@ -1,17 +1,29 @@
-# A script to install nano
+# A scipt to install homebrew
 
 if (which brew)
 then
- echo "brew is installed"
+echo "brew is installed"
 else
- echo "installing brew"
- /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo "installing brew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-if (which nano)
+# A script to install htop 
+
+if (which htop)
 then
+echo "htop is installed"
+else
+echo "installing htop"
+brew install htop
+fi
+
+# A script to install nano
+
+if (which nano)
+then 
  echo "nano is installed"
 else
  echo "installing nano"
- brew install nano
+brew install nano
 fi
