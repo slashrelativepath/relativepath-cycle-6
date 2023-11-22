f2801a851ab0707812e56845b373a640f47f0307
zaylon10 <149535109+zaylon10@users.noreply.github.com>
2023-11-22 00:02:26 +0000

    Add snap dependency

--- linux-webserver.sh
+++ linux-webserver.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
 # A script to build a webserver on linux
+if (apt-cache show snapd)
+then
+  echo "snapd already in apt-cache"
+else
+  echo "updating apt-cache"
+  sudo apt update
+fi
+
+if (snap --version)
+then
+  echo "snap already installed"
+else
+  echo "installing snap"
+  sudo apt install -y snapd
+  sleep 2
+fi
 
 if (multipass version)
 then
-  echo "multipass already intall"
+  echo "multipass already installed"
 else
-  echo "install multipass"
-  sudo snap install -y multipass
+  echo "installing multipass"
+  sudo snap install multipass
+  sleep 2
 fi
 
