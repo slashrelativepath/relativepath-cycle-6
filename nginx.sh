ea4963eb03eae94255d6846ea75535f10dde9b72
Christopher Forti <christopher.forti.79@gmail.com>
2023-12-01 22:42:30 -0500

    A possible path into nginx?

--- nginx.sh
+++ nginx.sh
@@ -7,4 +7,21 @@ else
   sudo apt install nginx
 fi
 
+# This worked for me on my system, but we should test...
+# Then wrap all code, in idempotent code blocks
 
+# Install build dependencies
+
+sudo apt install build-essential libpcre3 libpcre3-dev libssl-dev
+
+# download latest nginx source code
+
+wget http://nginx.org/download/nginx-1.23.3.tar.gz
+
+# Unpack source archive
+
+tar -zxvf nginx-1.23.3.tar.gz
+
+# Enter nginx source directory
+
+cd nginx-1.23.3
